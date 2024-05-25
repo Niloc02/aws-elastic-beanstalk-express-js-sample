feb3dbd7d10d10d306e30daa88658855ecfc1e40
Niloc02 <niloc02@gmail.com>
2024-05-25 17:21:48 -0500

    Fullpipelinetest2Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!   This is an updated message brought to you by the good folks at Github, through CodeBuild, CodePipeline  and AWS Elastic Beanstalk.  Tune in later for something more interesting. '));
+app.get('/', (req, res) => res.send('Hello World!   This is ANOTHER updated message brought to you by the good folks at Github, through CodeBuild, CodePipeline  and AWS Elastic Beanstalk.  Tune in later for something more interesting. '));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
